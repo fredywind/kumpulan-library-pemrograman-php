400a8c283b289b61d2c3c782c9ec379021b48e65
amalfathulloh <106308265+amalfathulloh@users.noreply.github.com>
2022-06-02 14:48:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,15 @@ function cetakhello() {
   echo "Hello World";
 # 00110287  
 }
-function cetakhello() {
-  echo "Hello World";
+function familyName($fname) {
+  echo "$fname Refsnes.<br>";
+}
+
+familyName("Jani");
+familyName("Hege");
+familyName("Stale");
+familyName("Kai Jim");
+familyName("Borge");
 # 19112464  
 }
 
