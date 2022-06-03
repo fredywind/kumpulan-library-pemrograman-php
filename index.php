cc0f72da60c95442bcca471c5e41c53a2bc2af76
Hilal3101 <106728452+Hilal3101@users.noreply.github.com>
2022-06-03 19:19:54 +0700

    Sistem sederhana pemakaian operator +, -, *, dan /

--- index.php
+++ index.php
@@ -4,7 +4,20 @@
 #    
 function cetakhello() {
   echo "Hello World";
-# 00110287  
+# 00110287
+# 19112479  
 }
-
+$a = 25;
+$b = 6;
+$c = $a + $b;
+echo "$a ditambah $b hasilnya $c<br />";
+$c = $a - $b;
+echo "$a dikurang $b hasilnya $c<br />";
+$c = $a * $b;
+echo "$a dikali $b hasilnya $c<br />";
+$c = $a / $b;
+echo "$a dibagi $b hasilnya $c<br />";
+$c = $a % $b;
+echo "$a dibagi $b sisanya $c<br />";
+echo "Di akhir program, a=$a, b=$b, c=$c";
 ?>
