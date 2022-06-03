bfa932df9d56d35019034d70080296cde4295d35
FerdiansyahHerlambang <106820497+FerdiansyahHerlambang@users.noreply.github.com>
2022-06-03 20:53:09 +0700

    Sistem Penggunaan operator Gabungan

--- index.php
+++ index.php
@@ -4,7 +4,14 @@
 #    
 function cetakhello() {
   echo "Hello World";
-# 00110287  
+# 00110287 
+# 19112475  
 }
-
+$a = 20;
+echo "a bernilai $a ditambah 30 = ".($a+=30)."<br />";
+echo "a sekarang $a kemudian dikurangi 20 = ".($a-=20)."<br />";
+echo "a sekarang $a dikalikan 3 = ".($a*=3)."<br />";
+echo "a sekarang $a dibagi 5 = ".($a/=5)."<br />";
+echo "a sekarang $a dibagi 4 sisanya = ".($a%=4)."<br />";
+echo "nilai a terakhir: $a";
 ?>
