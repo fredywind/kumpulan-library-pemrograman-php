92b855d9528c29cc30c4d351c07bc4c989695387
dimasmaulana13 <69634087+dimasmaulana13@users.noreply.github.com>
2022-06-11 14:31:33 +0700

    Update index.php
    Nama, NIM, Menghitung luas persegi panjang in Java language

--- index.php
+++ index.php
@@ -16,5 +16,20 @@ function tambah ()
   $y = 4;
   echo $x + $y;
 }
+#
+#
+# Dimas Maulana
+# 19112468
+import java.util.Scanner;
+public class LuasPersegi {
+  public static void main(String[] args) {
+    int sisi, luas;
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Masukkan panjang sisi persegi: ");
+    sisi = scan.nextInt();
+    luas = sisi * sisi;
+    System.out.println("Luas Persegi adalah " + luas);
+  }
+}
 ?>
 
